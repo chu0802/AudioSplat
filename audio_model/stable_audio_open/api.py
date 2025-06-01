1aae87fe5506be2dff3e45adbee43957966fb1f6
chu0802 <r09922104@ntu.edu.tw>
2025-06-01 14:43:10 +0800

    <update>: update api for stable audio open

--- audio_model/stable_audio_open/api.py
+++ audio_model/stable_audio_open/api.py
@@ -2,63 +2,50 @@ import torch
 import soundfile as sf
 from diffusers import StableAudioPipeline
 from pathlib import Path
+import json
+import argparse
+from tqdm import tqdm
 
-pipe = StableAudioPipeline.from_pretrained("stabilityai/stable-audio-open-1.0", torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--prompt_path", type=str, default="/home/chu980802/langsplat/audio_model/stable_audio_open/prompt.json")
+    parser.add_argument("-o", "--output_dir", type=str, default="output_test/stable_audio_open/")
+    parser.add_argument("-d", "--duration", type=float, default=10.0)
+    args  = parser.parse_args()
+    return args
 
+def main():
+    args = parse_args()
 
-negative_prompt = "Low quality."
-seed = 1102
+    pipe = StableAudioPipeline.from_pretrained("stabilityai/stable-audio-open-1.0", torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")
 
-# define the prompts
-# prompts = [
-#     "The sound of a wooden door opening",
-#     "The sound of a wooden door closing",
-#     "The sound of a wooden door creaking",
-#     "The sound of a wooden door slamming",
-#     "The sound of a wooden door knocking",
-#     "The sound of a wooden door hitting",
-#     "The sound of a wooden door pushing",
-# ]
+    negative_prompt = "Low quality."
+    seed = 1102
 
-# prompts = [
-#     "The sound of opening a window",
-#     "The sound of closing a window",
-#     "The sound of knocking a window",
-#     "The sound of rattling a window",
-#     "The sound of cleaning a window",
-#     "The sound of breaking a window",
-#     "The sound of shattering a window",
-#     "The sound of smashing a window",
-# ]
+    with open(args.prompt_path, "r") as f:
+        prompts = json.load(f)
 
-OBJECT_NAME = "kettle"
+    for object_name, prompt_list in prompts.items():
+        for prompt in tqdm(prompt_list):
+            generator = torch.Generator("cuda").manual_seed(seed)
 
-prompts = [
-    "The sound of boiling the kettle",
-    "The sound of whistling the kettle",
-    "The sound of pouring from the kettle",
-    "The sound of placing the kettle",
-    "The sound of lifting the kettle",
-]
+            # run the generation
+            audio = pipe(
+                prompt,
+                negative_prompt=negative_prompt,
+                num_inference_steps=100,
+                audio_end_in_s=args.duration,
+                num_waveforms_per_prompt=1,
+                generator=generator,
+            ).audios
 
-for prompt in prompts:
-    # set the seed for generator
-    generator = torch.Generator("cuda").manual_seed(seed)
+            output = audio[0].T.float().cpu().numpy()
+            output_dir = Path(args.output_dir) / object_name.replace(" ", "_")
+            output_dir.mkdir(parents=True, exist_ok=True)
+            
+            output_name = prompt.replace(" ", "_") + f"_{seed}.wav"
+            sf.write(output_dir / output_name, output, pipe.vae.sampling_rate)
 
-    # run the generation
-    audio = pipe(
-        prompt,
-        negative_prompt=negative_prompt,
-        num_inference_steps=100,
-        audio_end_in_s=10.0,
-        num_waveforms_per_prompt=3,
-        generator=generator,
-    ).audios
-
-    output = audio[0].T.float().cpu().numpy()
-    output_dir = Path("output") / "stable_audio_open" / OBJECT_NAME
-    output_dir.mkdir(parents=True, exist_ok=True)
-    
-    output_name = prompt.replace(" ", "_") + f"_{seed}.wav"
-    sf.write(output_dir / output_name, output, pipe.vae.sampling_rate)
+if __name__ == "__main__":
+    main()
